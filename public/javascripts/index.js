dc3c5dfe8c8a504e84e94f5ba873bcb7d247a12f
cuiere <ennaceur.safouen@gmail.com>
2017-12-20 06:07:11 +0100

    Infura + IPFS ok + dynamic async affectation

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -172,22 +172,6 @@ function getCountryInfo(cid){
        return contractInstance.methods.getCountryInfo(cid).call({gas:2000000});	 
 }
 
-
-
-
-/* $(document).ready(function() { 
-
-	contractInstance = new web3.eth.Contract(ABI_DEFINITION, CONTRACT_ADDRESS);
-	web3.eth.getAccounts()
-	  .then((accounts) => {
-	  console.log('token methods', contractInstance.methods.balanceOf(accounts[0]).call({gas:2000000}).then(console.log));
-	  })
-}); */
-
-
-//accounts[0]
-
-
 $(document).ready(function() { 
 		 if (typeof window.web3 === 'undefined') {
     // no web3, use fallback
@@ -332,5 +316,178 @@ $(document).ready(function() {
 		
 	
 	  });
+	  
+	  // Map declaration 
+	   
+	  
+	  var map;
+data_ = {};
+var colors =['#2ca1df','#DAF7A6','#FFC300','#FF5733','#C70039','#900C3F','#7b241c','#f0f3f4','#edbb99','#82e0aa','#808b96','#8e44ad'];
+var kings = {};
+var kings_name = [];
+var kings_refresh = [];
+var globalcont = 0;
+const countries_t = ["AFG","AGO","ALB","ARE","ARG","ARM","ATA","ATF","AUS","AUT","AZE","BDI","BEL","BEN","BFA","BGD","BGR","BHS","BIH","BLR","BLZ","BOL","BRA","BRN","BTN","BWA","CAF","CAN","CHE","CHL","CHN","CIV","CMR","COD","COG","COL","CRI","CUB","-99","CYP","CZE","DEU","DJI","DNK","DOM","DZA","ECU","EGY","ERI","ESP","EST","ETH","FIN","FJI","FLK","FRA","GUF","GAB","GBR","GEO","GHA","GIN","GMB","GNB","GNQ","GRC","GRL","GTM","GUY","HND","HRV","HTI","HUN","IDN","IND","IRL","IRN","IRQ","ISL","ISR","ITA","JAM","JOR","JPN","KAZ","KEN","KGZ","KHM","KOR","-99","KWT","LAO","LBN","LBR","LBY","LKA","LSO","LTU","LUX","LVA","MAR","MDA","MDG","MEX","MKD","MLI","MMR","MNE","MNG","MOZ","MRT","MWI","MYS","NAM","NCL","NER","NGA","NIC","NLD","NOR","NPL","NZL","OMN","PAK","PAN","PER","PHL","PNG","POL","PRI","PRK","PRT","PRY","QAT","ROU","RUS","RWA","ESH","SAU","SDN","SSD","SEN","SLB","SLE","SLV","-99","SOM","SRB","SUR","SVK","SVN","SWE","SWZ","SYR","TCD","TGO","THA","TJK","TKM","TLS","TTO","TUN","TUR","TWN","TZA","UGA","UKR","URY","USA","UZB","VEN","VNM","VUT","PSE","YEM","ZAF","ZMB","ZWE"]
+var countries = Datamap.prototype.worldTopo.objects.world.geometries;
+console.log('countries ==> ',countries);
+
+ map = new Datamap({element: document.getElementById('worldMap'),
+								responsive:true, 
+								 fills: {
+									 defaultFill:'#5d6d7e', 
+									 taken: '#FF0000'
+								 },
+								 data: data_ ,
+								 popupOnHover: true,
+								 geographyConfig: {
+										popupTemplate: function(geo, data) {
+											return ['<div class="hoverinfo"><strong>',
+													'Country ' + geo.properties.name+ '</div>'].join(''); // should return the owner
+										}
+									}
+								 });
+	var zabab = {}; 
+	var countries_visited = 0;
+	  
+	  function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function update (k){
+	map.updateChoropleth(k);
+	}
+	  
+for (var cc=0; cc< countries_t.length; cc ++){
+	 
+	 
+	 // async tnéket omha 
+		  contractInstance.methods.getCountryInfo(Web3.utils.asciiToHex(countries_t[cc])).call({gas:2000000},function(err,res){
+			 console.log('calling node for countryinformation');
+			 		 	if(!err && res[3]){
+							id = res[0];
+							king = res[1];
+							price = res[2];
+							taken = res[3];
+							
+							if (zabab[king] != undefined){
+								
+								console.log('daznamou ',map)
+							}
+							else{
+							tmp_r = {};
+							zabab[king] = getRandomColor()
+							key = Web3.utils.toUtf8(id)
+							console.log('zabab[king].toString ',zabab[king])
+							tmp_r[key] = zabab[king]
+							update(tmp_r );
+								
+							}
+							
+							// filling kings
+							//console.log('kings[king]79 ',kings[king] );
+							if(kings[king] == undefined){
+								kings_name.push(king);
+								kings[king] = [];
+								console.log('Web3.utils.toUtf8(id) ',Web3.utils.toUtf8(id))
+								console.log('kings[king]9 ',king );
+
+								//kings[king].push(Web3.utils.toUtf8(id));
+							}
+								console.log('kings[king] ',king ,id);
+							//console.log('kings[king] ',kings[king] );
+							//console.log('Web3.utils.toUtf8(id)2 ',Web3.utils.toUtf8(id))
+							kings[king].push(Web3.utils.toUtf8(id));
+							
+							var tkn = ""
+							if (taken == true )
+								tkn = 'taken'
+							 
+						 
+						  data_[countries_t[cc]]={
+							 'fillKey':tkn,//taken.toString(),
+							 'king':king,
+							 'price':price,
+							 'soldiers':0}	 
+		                } // End if !err && res[3] ! taken
+		 });
+
+		 globalcont++;
+			 
+			 
+ }; // End foreach
+
+
+
+	 
+console.log('data ', kings[kings_name[0]]); // id => [id_country]
+	var ci = 0; // internal counter just to this func
+	kings_name.forEach((name)=>{
+		kings[name].forEach((country)=>{
+			var country_cc = {};
+			country_cc[country] =colors[ci] 
+			kings_refresh.push(country_cc);
+			
+		});
+		ci++;
+	});
+	
+ 
+ 
+
+	
+					
+     kings_refresh.forEach((e)=>{					
+	map.updateChoropleth(e);
+	 });
+
+								 
+	console.log('constructedMAp ',kings_refresh);
+								 
+	// define onclick for the map
+	map.svg.selectAll('.datamaps-subunit').on('click', function(data) {
+	contractInstance.methods.getCountryInfo(Web3.utils.asciiToHex(data.id.toString())).call({gas:2000000},function(err,res){
+		if(!err){
+		id = res[0];
+		king = res[1];
+		price = res[2];
+		taken = res[3];
+		console.log('debug ',globalcont)
+		
+		if (taken){
+					$('#countryid').val(Web3.utils.toUtf8(id));
+					$('#countryking').val(king);
+					$('#countryprice').val(price);
+					$('#countrysoldiers').val(taken);
+					$('#takenmodal').modal('show');
+					}
+					else {
+						$('#emptycountryid').val(data.id);
+						$('#emptycountryking').val("N/A Not yet Conquested");
+						$('#emptycountryprice').val(1);
+						$('#emptycountrysoldiers').val(0);
+						$('#emptymodal').modal('show');
+					}
+
+
+					}
+				});	
+					
+    });// end Onclick
+			
+
+    $(window).on('resize', function() {
+       map.resize();
+    });
+	
+    console.log('mapdd ', countries);
+
+
+		
+		
   
 });
